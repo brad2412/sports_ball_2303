54b86d62e2edd4bbc7a5585ee63117f612c742d6
Bradley Milton <bmilton2412@gmail.com>
2023-03-29 09:12:04 -0700

    fist and last name added

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,21 @@
 class Player
-  def initialize(name, monthly_cost, contract_length)
+  attr_reader :name, : monthly_cost, :contract_length, :nick_name
+
+  def initialize(name, monthly_cost, contract_length, nick_name)
+    @name = name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @nick_name = nick_name
   end
+
+    def first_name
+      name.split[0]
+    end
+
+    def last_name
+      name.split[1]
+    end
+
+      
 end
 
